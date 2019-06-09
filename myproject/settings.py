78da3f6f0febd40a4a876bf2f61d88ccf26c9996
MukundKulkarni <mukundkulkarni786@gmail.com>
2019-06-09 11:44:05 +0530

    Solved the static files not loading issue

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%%(qol66ho#+x1p9peq3r@97v6yo48i-go$g)sjz6_2*$s=foq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['amanjhurani.pythonanywhere.com']
+ALLOWED_HOSTS = ['amanjhurani.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -118,4 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = './static/'
+STATIC_URL = '/static/'
+#STATIC_ROOT= os.path.join(BASE_DIR,'/static/') to be used during prodiction
